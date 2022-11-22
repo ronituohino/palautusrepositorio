d23dc51d49c041a9d93ff2c940e5270e67c40007
Roni Tuohino <tuohinoroni@gmail.com>
2022-11-22 18:33:05 +0200

    add: can add products to basket

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,25 +3,19 @@ from ostos import Ostos
 
 
 class Ostoskori:
-    def __init__(self, ostokset=[]):
-        self.ostos_list = ostokset
-        pass
-        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+    def __init__(self, tuotteet: list[Tuote] = []):
+        self.ostos_list = []
+        for t in tuotteet:
+            self.ostos_list.append(Ostos(t))
 
     def tavaroita_korissa(self):
         return len(self.ostos_list)
-        pass
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
 
     def hinta(self):
         return 0
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        self.ostos_list.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
